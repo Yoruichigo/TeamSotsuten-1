75eb120adb119a00a43cfd7d397e92c4659cfa61
syougun360 <syougun360@infoseek.jp>
2016-01-26 23:36:33 +0900

    サウンドのマスタリング

--- Assets/Script/Audio/AudioManager.cs
+++ Assets/Script/Audio/AudioManager.cs
@@ -10,9 +10,9 @@ namespace Audio
          DAMAGE,  //<Damage
          DECISION,  //<Decision
          ENEMYAPPEARANCE,  //<EnemyAppearance
-         ENEMYATTACK,  //<EnemyAttack
          ENEMYHIT,  //<EnemyHit
          ENEMYSIREN,  //<EnemySiren
+         GOLEMATTACK,  //<GolemAttack
          PLAYERSTRENGTHATTACK,  //<PlayerStrengthAttack
          PLAYERWEAKATTACK,  //<PlayerWeakAttack
      }
@@ -29,19 +29,18 @@ public class SEAudioData
      public string label;
      public Audio.SEID id;
 }
-
 public class AudioManager
 {
      static public SEAudioData []SEData = new SEAudioData[]
      {
-         new SEAudioData(){label = "Damage",         id = Audio.SEID.DAMAGE}, 
-         new SEAudioData(){label = "Decision",         id = Audio.SEID.DECISION}, 
-         new SEAudioData(){label = "EnemyAppearance",         id = Audio.SEID.ENEMYAPPEARANCE}, 
-         new SEAudioData(){label = "EnemyAttack",         id = Audio.SEID.ENEMYATTACK}, 
-         new SEAudioData(){label = "EnemyHit",         id = Audio.SEID.ENEMYHIT}, 
-         new SEAudioData(){label = "EnemySiren",         id = Audio.SEID.ENEMYSIREN}, 
-         new SEAudioData(){label = "PlayerStrengthAttack",         id = Audio.SEID.PLAYERSTRENGTHATTACK}, 
-         new SEAudioData(){label = "PlayerWeakAttack",         id = Audio.SEID.PLAYERWEAKATTACK}, 
+         new SEAudioData(){label ="Damage",         id = Audio.SEID.DAMAGE}, 
+         new SEAudioData(){label ="Decision",         id = Audio.SEID.DECISION}, 
+         new SEAudioData(){label ="EnemyAppearance",         id = Audio.SEID.ENEMYAPPEARANCE}, 
+         new SEAudioData(){label ="EnemyHit",         id = Audio.SEID.ENEMYHIT}, 
+         new SEAudioData(){label ="EnemySiren",         id = Audio.SEID.ENEMYSIREN}, 
+         new SEAudioData(){label ="GolemAttack",         id = Audio.SEID.GOLEMATTACK}, 
+         new SEAudioData(){label ="PlayerStrengthAttack",         id = Audio.SEID.PLAYERSTRENGTHATTACK}, 
+         new SEAudioData(){label ="PlayerWeakAttack",         id = Audio.SEID.PLAYERWEAKATTACK}, 
      };
 
      static public BGMAudioData []BGMData = new BGMAudioData[]
