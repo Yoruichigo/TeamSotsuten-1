95d0ad8001bdd3ae7318c3a1c49ff1ecafd2cb6a
unknown <nonke4352_114514@outlook.com>
2016-01-27 20:09:57 +0900

    エネミーのHPバー改良

--- Assets/Enemy/EnemyHelthVar.cs
+++ Assets/Enemy/EnemyHelthVar.cs
@@ -5,9 +5,6 @@ using System.Collections;
 
 public class EnemyHelthVar : MonoBehaviour 
 {
-    [SerializeField]
-    EnemyData TargetEnamy;      //敵
-
     Image HelthVar;
 
     int lifeMaX;//体力
@@ -15,18 +12,23 @@ public class EnemyHelthVar : MonoBehaviour
 	// Use this for initialization
 	void Start () 
     {
-        lifeMaX = TargetEnamy.Life;
+        SetMaxLife();
         HelthVar = GetComponent<Image>();
 	}
-	
+
+    void SetMaxLife()
+    {
+        lifeMaX = EnemyManager.Instance.GetActiveEnemyData().Life;
+    }
+
 	// Update is called once per frame
 	void Update () 
     {
 
-        if (TargetEnamy.IsActive())
+        if (EnemyManager.Instance.GetActiveEnemyData().IsActive())
         {
             HelthVar.enabled = true;
-            HelthVar.fillAmount = TargetEnamy.Life / lifeMaX;
+            HelthVar.fillAmount = EnemyManager.Instance.GetActiveEnemyData().Life / lifeMaX;
         }
         else 
         {
