f923c6846ffc62b9b8dbfc05f7dc5fff4b752d50
unknown <nonke4352_114514@outlook.com>
2016-01-27 19:06:42 +0900

    リザルト画面表示

---
+++ Assets/Script/Result/ResultWindowMover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultWindowMover : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+        iTween.RotateTo(this.gameObject, iTween.Hash("x", 180,"time", 3));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
