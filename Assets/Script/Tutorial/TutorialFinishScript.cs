afefccb4cbfdd1038070ab823f168427d5565f9b
Yoruichigo <lifeitigomasyumaro@yahoo.co.jp>
2016-02-13 12:28:18 +0900

    Tutorial bugfix and adjustment

--- Assets/Script/Tutorial/TutorialFinishScript.cs
+++ Assets/Script/Tutorial/TutorialFinishScript.cs
@@ -9,6 +9,8 @@ public class TutorialFinishScript : MonoBehaviour {
     [SerializeField]
     GameObject ImageObj;
 
+    [SerializeField]
+    string endWaitTweenName = "";
 
     string tweenNameIn = "TutorialFinishIn";
     string tweenNameOut = "TutorialFinishOut";
@@ -30,15 +32,30 @@ public class TutorialFinishScript : MonoBehaviour {
             SubFunc = null;
         }
     }
+
+    void DelayStartFunc()
+    {
+        if (!uTween.IsPlaying(endWaitTweenName))
+        {
+            uTween.Play(tweenNameIn);
+            ImageObj.SetActive(true);
+            SubFunc = null;
+        }
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
 
+        if (null != SubFunc)
+        {
+            SubFunc();
+        }
+
         switch (TutorialSequence.GetNowState())
         {
             case TutorialSequence.State.ON_FINISH_GUID:
-                ImageObj.SetActive(true);
-                uTween.Play(tweenNameIn);
+                SubFunc = DelayStartFunc;
                 break;
             case TutorialSequence.State.FINISH_GUID:
 
